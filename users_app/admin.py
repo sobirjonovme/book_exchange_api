972a04e40a191599e9d03ce36aafa515aa1cfb59
sobirjonovme <huntsman6004@gmail.com>
2023-02-04 17:22:47 +0500

    initial commit

---
+++ users_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from users_app.models import CustomUser
+
+
+# Register your models here.
+admin.site.register(CustomUser)
---
+++ users_app/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from rest_framework.authtoken.models import Token
+
+
+# Create your models here.
+class CustomUser(AbstractUser):
+
+    def get_tokens(self):
+        token, created = Token.objects.get_or_create(user=self)
+        data = {
+            'token': token.key,
+        }
+
+        return data
