2d17e1347e17d754c0f7c2e0cce39ab78a99c00a
sobirjonovme <huntsman6004@gmail.com>
2023-02-05 16:35:22 +0500

    added: Book model to books_app, Pillow to requirements.txt

--- books_app/models.py
+++ books_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+from users_app.models import CustomUser
+
+EXIST, NEED = ('exist', 'need')
+
+
 # Create your models here.
+class Book(models.Model):
+
+    BOOK_STATUS = (
+        (EXIST, EXIST),
+        (NEED, NEED)
+    )
+
+    owner = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='user_books'
+    )
+    status = models.CharField(max_length=10, choices=BOOK_STATUS)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    image = models.ImageField()
+    description = models.TextField()
