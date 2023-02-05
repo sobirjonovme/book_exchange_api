27da11430e8c6f56ad1bfe403638aa8bd8bb4ede
sobirjonovme <huntsman6004@gmail.com>
2023-02-05 16:19:28 +0500

    added: region field to CustomUser model

--- users_app/models.py
+++ users_app/models.py
@@ -4,11 +4,24 @@ from django.db import models
 
 from rest_framework.authtoken.models import Token
 
+KARARAKALPAKISTAN, ANDIJAN, BUKHRARA, JIZZAX, QASHQADARYO, NAVOIY, NAMANGAN,\
+    SAMARKAND, SURXONDARYO, SIRDARYO, TASHKENT_CITY, TASHKENT_REGION, FERGANA, XORAZM = \
+    ("Qoraqalpogʻiston", "Andijon", "Buxoro", "Jizzax", "Qashqadaryo", "Navoiy", "Namangan",
+     "Samarqand", "Surxondaryo", "Sirdaryo", "Toshkent shahri", "Toshkent viloyati", "Fargʻona", "Xorazm"
+     )
+
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    REGIONS = (
+        (KARARAKALPAKISTAN, KARARAKALPAKISTAN), (ANDIJAN, ANDIJAN), (BUKHRARA, BUKHRARA), (JIZZAX, JIZZAX),
+        (QASHQADARYO, QASHQADARYO), (NAVOIY, NAVOIY), (NAMANGAN, NAMANGAN), (SAMARKAND, SAMARKAND),
+        (SURXONDARYO, SURXONDARYO), (SIRDARYO, SIRDARYO), (TASHKENT_CITY, TASHKENT_CITY),
+        (TASHKENT_REGION, TASHKENT_REGION), (FERGANA, FERGANA), (XORAZM, XORAZM)
+    )
 
     first_name = models.CharField(_("first name"), max_length=150)
+    region = models.CharField(max_length=30, choices=REGIONS, default=TASHKENT_CITY)
 
     def get_tokens(self):
         token, created = Token.objects.get_or_create(user=self)
