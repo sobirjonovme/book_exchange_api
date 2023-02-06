4e47aac50b69170304b153c9629291db897ff621
sobirjonovme <huntsman6004@gmail.com>
2023-02-06 11:11:29 +0500

    added: BookAdmin

--- books_app/admin.py
+++ books_app/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from books_app.models import Book
 
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'status', 'owner')
+    list_filter = ('status', )
+
+
 # Register your models here.
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
