4eb63b1edec47b2ac008205daeec63d12ed55c14
sobirjonovme <huntsman6004@gmail.com>
2023-02-07 18:39:36 +0500

    added: BookExchange and EndExchangeRequest models

--- book_exchange_app/models.py
+++ book_exchange_app/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.utils import timezone
 
-
 from users_app.models import CustomUser
 from books_app.models import Book
+
+ONGOING, END = ('ongoing', 'end')
+BOOK_EXCHANGE_STATUS = (
+    (ONGOING, ONGOING),
+    (END, END)
+)
 
 
 # Create your models here.
@@ -16,6 +21,42 @@ class BookRequest(models.Model):
     from_user = models.ForeignKey(
         CustomUser,
         on_delete=models.CASCADE,
-        related_name='requests_from_user'
+        related_name='book_requests_from_user'
     )
     created_at = models.DateTimeField(default=timezone.now)
+
+
+class BookExchange(models.Model):
+    book1 = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='book_exchanges_1'
+    )
+    book2 = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='book_exchanges_2'
+    )
+    status = models.CharField(max_length=10, choices=BOOK_EXCHANGE_STATUS, default=ONGOING)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class EndExchangeRequest(models.Model):
+    exchange = models.ForeignKey(
+        BookExchange,
+        on_delete=models.CASCADE,
+        related_name='end_exchange_requests'
+    )
+    from_user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='end_exchange_requests_from_user'
+    )
+    to_user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='end_exchange_requests_to_user'
+    )
+
+
+
